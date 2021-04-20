498dd93ea5564def9be81c9a038c0c31f2d5c27c
PunreachFNSV <punreachrany@fnsvalue.co.kr>
2021-04-20 16:40:01 +0900

    Flash Chat Study

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by elite on 2021/04/20.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+    
+}
